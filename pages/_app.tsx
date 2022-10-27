ea9158ec6245825b7d8e2dd7eec6614790b03d62
Alex Yang <alexalexyang@gmail.com>
2022-10-27 16:24:23 +0200

    Add manifest and icons for PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,7 +7,10 @@ export default function App({ Component, pageProps }: AppProps) {
     <Head>
       <title>Vapour</title>
       <meta name="description" content="Vapour by Supermeowkat" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/logo-512x512-white-bg.png" />
+
+      <link rel="manifest" href="/manifest.json" />
+
       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css"
         integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossOrigin='' />
     </Head>
